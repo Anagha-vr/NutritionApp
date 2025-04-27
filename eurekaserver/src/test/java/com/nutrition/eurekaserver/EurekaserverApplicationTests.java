e719dc459e735fe64616c1ed7b45588135d5de83
Anagha-vr <anagha2082@gmail.com>
2025-04-27 19:07:18 +0530

    Add files via upload

---
+++ eurekaserver/src/test/java/com/nutrition/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrition.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
